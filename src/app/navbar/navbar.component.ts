93edb4b04991e5995e076ae65e68eed8b28f8741
Gel D <lfagel84@gmail.com>
2022-01-25 07:05:30 -0500

    add mobile responsive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  isMobile = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openNavMenu(){
+    console.log("clicked open nav");
+    this.isMobile = !this.isMobile;
   }
 
 }
